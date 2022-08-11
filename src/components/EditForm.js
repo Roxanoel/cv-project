fd1bbb45057575e29d0dc82fc097954f236caa91
Roxanoel <roxane.noel92@gmail.com>
2022-08-11 11:10:51 +0100

    Added main structure for EditForm's JSX.

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -3,11 +3,18 @@ import React, { Component } from "react";
 export class EditForm extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+
+    }
   }
 
   render() {
     return (
-        <div />
+        <form>
+            {/* sub-forms will be here */}
+            <button>Submit</button> 
+        </form>
     )
   }
 }
