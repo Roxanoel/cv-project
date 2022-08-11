20f802868debda622de1576ee630717636a2348c
Roxanoel <roxane.noel92@gmail.com>
2022-08-11 11:43:24 +0100

    PersonalInfo structure + basic event handling.

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -6,14 +6,29 @@ export class EditForm extends Component {
     super(props);
 
     this.state = {
-
+        name: '',
+        email: '',
+        phone: '',
     }
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.updateState = this.updateState.bind(this);
+  }
+
+  handleOnChange(e) {
+    // Update the value in state
+    this.updateState(e.target.id, e.target.value);
+  }
+
+  updateState(propertyName, value) {
+    console.log(propertyName + ': ' + value);
   }
 
   render() {
     return (
         <form>
             {/* sub-forms will be here */}
+            <PersonalInfo handleOnChange={this.handleOnChange} />
             <button>Submit</button> 
         </form>
     )
--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -3,15 +3,20 @@ import React, { Component } from "react";
 export class PersonalInfo extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-
-    }
   }
 
   render() {
+    const { handleOnChange } = this.props;
     return (
-        <div />
+        <fieldset>
+          <h3>Personal Information</h3>
+          <label htmlFor="name">Name: </label>
+          <input id="name" name="name" onChange={handleOnChange}></input>
+          <label htmlFor="email">Email: </label>
+          <input id="email" name="email" type="email" onChange={handleOnChange}></input>
+          <label htmlFor="phone-">Phone number: </label>
+          <input id="phone" name="phone" onChange={handleOnChange}></input>
+        </fieldset>
     )
   }
 }
