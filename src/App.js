46743b2b9024b486b4e303ae23ffe65346198625
Roxanoel <roxane.noel92@gmail.com>
2022-08-11 11:05:25 +0100

    Turned App into a class component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { Component } from "react";
 import { EditForm } from "./components/EditForm";
 
-function App() {
-  return (
-    <div>
-      <EditForm></EditForm>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <EditForm></EditForm>
+      </div>
+    );
+  }
 }
 
 export default App;
