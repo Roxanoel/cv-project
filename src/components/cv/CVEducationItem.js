35e81ed38dc06905eee7c9f26b5afb337d9f3e0b
Roxanoel <roxane.noel92@gmail.com>
2022-08-15 10:39:46 +0100

    Added basic JSX for CVEducationItem

--- src/components/cv/CVEducationItem.js
+++ src/components/cv/CVEducationItem.js
@@ -6,10 +6,25 @@ export class CVEducationItem extends Component {
     }
 
     render() {
-        return (
-            <div>
+        const {
+            id, 
+            institution, 
+            degree, 
+            start, 
+            end, 
+            gpa} = this.props;
 
-            </div>
+        return (
+            <ul>  {/* Will likely be some kind of grid? + add labels later */}
+                {/* Institution */}
+                <li key={`${id}-institution`}>{institution}</li>
+                {/* Degree */}
+                <li key={`${id}-degree`}>{degree}</li>
+                {/* start + end */}
+                <li key={`${id}-dates`}>{`${start}-${(end === '') ? end : '...'}`}</li>
+                {/* gpa */}
+                <li key={`${id}-gpa`}>{gpa}</li>
+            </ul>
         )
     };
 }
